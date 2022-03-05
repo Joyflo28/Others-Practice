341b2d8ee4020b9b3822e00e7970e842009ddc5e
Andres Flores <floresguerreroandresjose@gmail.com>
2022-03-05 08:26:44 -0400

    agregando fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: "assets/image/[hash][ext][query]"
     },
     resolve: {
         extensions: ['.js']
@@ -23,6 +24,24 @@ module.exports = {
             {
                 test: /\.css|.scss$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff2|woff)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/font/",
+                        esModule: false
+                    },
+                }
             }
         ]
     },
